bc514ef77b069a7456481d90e76b40f6d70de8d5
Hiba Louzzani <jh_louzzani@esi.dz>
2023-05-05 19:26:10 +0100

    Added color green to config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,32 @@
-/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {  fontFamily: {
-      
-          'mons': ['Montserrat', 'sans-serif']
-        },},
-        colors: {
-          CustomGreen: '#59C55E',
-        },
+    extend: {
+      fontFamily: {
+        'mons': ['Montserrat', 'sans-serif']
+      },
+      colors: {
+        CustomGreen: '#59C55E',
+      },
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      red: colors.red,
+      yellow: colors.amber,
+      green: colors.emerald,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      purple: colors.violet,
+      pink: colors.pink,
+      green:colors.green,
+    },
   },
+  variants: {},
   plugins: [],
 };
